f03a9e983d1eb1418ea7e4c0652549950cfc84bf
Shubham Chougule <89000326+CosmicStrike@users.noreply.github.com>
2023-11-23 19:39:06 +0530

    Add Events

---
+++ src/pages/events_past.jsx
@@ -0,0 +1,10 @@
+import Events_Navbar from "../components/events_navbar";
+export default function Events_Past() {
+    return (
+        <>
+            <Events_Navbar current={2} />
+
+            Past
+        </>
+    );
+} 
