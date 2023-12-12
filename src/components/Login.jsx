16f5abc9d6340a7555ebd92b9c630ef2d9cbcd00
Ayush Deshpande <ayushdeshpande81@gmail.com>
2023-12-12 17:24:58 +0000

    Update registration and login components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,20 +3,20 @@ export default function Login(){
         <div className="flex flex-col">
             <div className="flex flex-row items-center justify-center pt-4">
 
-                <div className="flex flex-col pt-5">
-                    <div className="flex flex-row items-center justify-center">
+            <div className="flex flex-col pt-16 items-center justify-center w-full">
+                    <div className="flex flex-row pr-4">
 
-                    <label htmlFor="role" className={
-                        "px-4 py-2 rounded-l-lg w-48 lg:w-64"
-                        }>Login</label>
-                    <select name="role" id="role">
-                        <option value="student">Student</option>
-                        <option value="alumni">Alumni</option>
-                    </select>
+                        <label htmlFor="role" className={
+                            "px-4 rounded-l-lg w-48 lg:w-64 font-bold"
+                            }>Login Type</label>
+                        <select name="role" id="role" className="pr-4">
+                            <option value="student">Student</option>
+                            <option value="alumni">Alumni</option>
+                        </select>
                     </div>
-                <input type="text" placeholder="Email" className="border-2 border-black" />
-                <input type="password" placeholder="Password" className="border-2 border-black" />
-                <button className="bg-primary text-white px-4 py-2 rounded-lg">Login</button>
+                <input type="text" placeholder="Email" className="border-2 border-black w-[60%] p-1 mt-16" />
+                <input type="password" placeholder="Password" className="border-2 border-black w-[60%] p-1 mt-8" />
+                <button className="bg-primary text-white px-4 py-2 rounded-lg mt-16 w-32 font-bold">Login</button>
                 </div>
 
             
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,22 +3,32 @@ export default function Login(){
         <div className="flex flex-col">
             <div className="flex flex-row items-center justify-center pt-4">
 
-                <div className="flex flex-col pt-5">
-                    <div className="flex flex-row items-center justify-center">
+            <div className="flex flex-col pt-16 items-center justify-center w-full">
+                    <div className="flex flex-row pr-4">
 
-                    <label htmlFor="role">Register</label>
-                    <select name="role" id="role">
-                        <option value="student">Student</option>
-                        <option value="alumni">Alumni</option>
-                    </select>
+                        <label htmlFor="role" className={
+                            "px-4 rounded-l-lg w-48 lg:w-64 font-bold"
+                            }>Registration Type</label>
+                        <select name="role" id="role" className="pr-4">
+                            <option value="student">Student</option>
+                            <option value="alumni">Alumni</option>
+                        </select>
                     </div>
-                <input type="text" placeholder="Email" className="border-2 border-black" />
-                <input type="password" placeholder="Password" className="border-2 border-black" />
-                <button className="bg-primary text-white px-4 py-2 rounded-lg">Login</button>
+
+                <div className="inputs flex flex-col items-center justify-center pt-16 w-full">
+                    <div className="flex flex-row justify-between w-[60%]">
+                        <input type="text" placeholder="Name" className="border-2 border-black w-full p-1 mr-2" />
+                        <input type="email" placeholder="Email" className="border-2 border-black w-full p-1 ml-2" />
+                    </div>
+                    <input type="text" placeholder="CPRN" className="border-2 border-black w-[60%] p-1 mt-8" />
+                    <input type="password" placeholder="Password" className="border-2 border-black w-[60%] p-1 mt-8" />
+                    <input type="password" placeholder="Confirm Password" className="border-2 border-black w-[60%] p-1 mt-8" />
+                    <button className="bg-primary text-white px-4 py-2 rounded-lg mt-16 w-32 font-bold">Login</button>
                 </div>
+            </div>
 
             
-            </div>
         </div>
+    </div>
     )
 }
