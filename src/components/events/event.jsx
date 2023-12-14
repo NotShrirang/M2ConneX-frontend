8032f4cfa604d81c7acc8606247e4edb15b97984
Shubham Chougule <89000326+CosmicStrike@users.noreply.github.com>
2023-12-14 12:59:02 +0530

    Update event nacbar, Add event brief

---
+++ src/components/events/event.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import arrows from '../../assets/arrow.svg'
+export default function Event({ upcoming = false }) {
+    return (
+        <div className="w-[100%] h-[27rem] mb-[6.813rem] flex flex-row">
+            <div className="w-[42%] h-[100%] mr-[5%]">
+                <img className="w-[100%] h-[100%] rounded-lg shadow-md" />
+            </div>
+            <div className="w-[55%] h-[100%] flex flex-col">
+                <div className="w-[100%] h-[65%]  overflow-hidden text-justify leading-event text-[1.5rem]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla nemo, necessitatibus delectus expedita architecto facere qui animi, a porro veritatis asperiores sed ea dicta iste officia itaque ipsa maxime tempora quisquam voluptateslpa? Commodi necessitatibus reprehenderit sit deleniti mollitia!
+                </div>
+                <Link className="w-[12rem] self-end flex flex-row justify-end items-center text-[1.5rem]">
+                    <div className="text-lightblue my-5"> know more</div>
+                    <div className="w-8 h-1"><img src={arrows} /></div>
+                </Link>
+                {upcoming &&
+                    <button className="ml-[40%] w-[11rem] h-[4.375rem] text-[2.5rem] font-bold text-white rounded-md bg-lightmaroon text-center shadow-md shadow-gray ">RVSP</button>
+                }
+            </div>
+        </div>
+
+    )
+}
--- src/components/events/events_navbar.jsx
+++ src/components/events/events_navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function Events_Navbar({ current = 1 }) {
     return (
         <>
-            <div className="flex items-center w-[75rem] h-[4rem] bg-black rounded-3xl mx-auto mt-[5rem] cursor-pointer">
-                <Link to={'/events/upcoming'} className={`w-[25rem] h-[4rem] pt-1 text-center text-[2rem] text-white font-semibold rounded-3xl ${current === 1 ? 'bg-primary' : 'bg - black'}`}>Upcoming Events</Link>
-                <Link to={'/events/past'} className={`w-[25rem] h-[4rem] pt-1 text-center text-[2rem] text-white font-semibold rounded-3xl ${current === 2 ? 'bg-primary' : 'bg - black'}`}> Past Events</Link>
-                <Link to={'/events/gallery'} className={`w-[25rem] h-[4rem] pt-1 text-center text-[2rem] text-white font-semibold rounded-3xl ${current === 3 ? 'bg-primary' : 'bg - black'}`}> Phote Gallery</ Link>
+            <div className="flex items-center w-[100%] h-[4rem] bg-primary rounded-3xl mx-auto mt-[5rem] mb-[5rem] cursor-pointer font-bold">
+                <Link to={'/events/upcoming'} className={`w-[25rem] h-[4rem] pt-1 text-center text-[2rem] rounded-3xl ${current === 1 ? 'bg-black text-white' : ''}`}>Upcoming Events</Link>
+                <Link to={'/events/past'} className={`w-[25rem] h-[4rem] pt-1 text-center text-[2rem] rounded-3xl ${current === 2 ? 'bg-black text-white' : ''}`}> Past Events</Link>
+                <Link to={'/events/gallery'} className={`w-[25rem] h-[4rem] pt-1 text-center text-[2rem] rounded-3xl ${current === 3 ? 'bg-black text-white' : ''}`}> Phote Gallery</ Link>
             </div >
         </>
     )
