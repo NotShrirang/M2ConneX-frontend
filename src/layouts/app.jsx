d31affd56f8ecf507c6e2589dccd88a643cdb5ec
Sammy429b <samarthnbahirgonde49@gmail.com>
2023-12-23 21:28:20 +0530

    Navbar design updated

--- src/layouts/app.jsx
+++ src/layouts/app.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import Nav from "../components/Nav";
 const App_Layout = () => {
     return (
         <div className="h-screen">
             <Header />
+            <Nav />
             <Outlet />
             <Footer />
         </div>
